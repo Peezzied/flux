83fd53224881b098974ea3b5e4ed95dbdb61fab1
BuildTools <unconfigured@null.spigotmc.org>
2024-03-06 01:44:37 +0800

    VideoSection improvement

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -11,7 +11,7 @@ export default function VideoSection({ data, isReversed = false, isBoxed, backgr
     })
     return (
 
-        <DivBox id={id} div={background ? `h-full bg-no-repeat bg-center bg-cover relative ${isReversed ? 'after:sectionOverlayLeft' : 'after:sectionOverlayRight'} after:bgOverlay group ` : "  group relative bg-gradient-to-l to-[#59BDFF] from-[#F8F8F8] to-[230%] from-[50%]"} container={isBoxed && 'container'} style={background ? { backgroundImage: `url(${data.img})` } : {}} {...props}>
+        <DivBox id={id} div={background ? `h-full bg-no-repeat bg-center bg-cover relative sectionOverlayTop ${isReversed ? 'xl:after:sectionOverlayLeft' : 'xl:after:sectionOverlayRight'} after:bgOverlay group ` : "  group relative bg-gradient-to-l to-[#59BDFF] from-[#F8F8F8] to-[230%] from-[50%]"} container={isBoxed && 'container'} style={background ? { backgroundImage: `url(${data.img})` } : {}} {...props}>
             <div className={`flex items-center justify-center p-12 sm:px-12 px-5 py-16 mx-auto max-w-[110em] gap-16 xl:gap-28 relative z-10 flex-col ${isReversed ? 'xl:flex-row-reverse' : 'xl:flex-row'}`}>
 
                 <div className={`${background ? 'text-white' : ''} xl:max-w-[29em]`}>
